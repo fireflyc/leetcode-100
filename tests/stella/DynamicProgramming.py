4c8bb0370a77b3537c9fbb652837ecbc261b313e
诸娟娟(Zhu Juanjuan) <juanjuan.zhu@gds-services.com>
2021-05-08 14:17:42 +0800

    tests

---
+++ tests/stella/DynamicProgramming.py
@@ -0,0 +1,19 @@
+import unittest
+
+from leetcode.stella import DynamicProgramming
+
+
+class DynamicProgrammingTestCase(unittest.TestCase):
+    def test_house_robber(self):
+        self.assertEqual(4, DynamicProgramming.house_robber([1, 2, 3, 1]))
+        self.assertEqual(22, DynamicProgramming.house_robber([2, 1, 9, 20, 1]))
+        self.assertEqual(10, DynamicProgramming.house_robber([2, 1, 1, 7, 1, 1]))
+
+    def test_coin_change(self):
+        self.assertEqual(3, DynamicProgramming.coin_change([1, 2, 5], 11))
+        self.assertEqual(5, DynamicProgramming.coin_change([2, 3, 5, 7], 31))
+        self.assertEqual(6, DynamicProgramming.coin_change([1, 2, 5, 7], 31))
+        self.assertEqual(-1, DynamicProgramming.coin_change([2, 5, 7], 31))
+        self.assertEqual(-1, DynamicProgramming.coin_change([2], 3))
+        self.assertEqual(0, DynamicProgramming.coin_change([1], 0))
+        self.assertEqual(100, DynamicProgramming.coin_change([1], 100))
--- tests/xs/DynamicProgramming.py
+++ tests/xs/DynamicProgramming.py
@@ -1,7 +1,6 @@
 import unittest
 
 from leetcode.xs import DynamicProgramming
-from leetcode.stella import DynamicProgramming as dp
 
 
 class DynamicProgrammingTestCase(unittest.TestCase):
@@ -9,9 +8,6 @@ class DynamicProgrammingTestCase(unittest.TestCase):
         self.assertEqual(4, DynamicProgramming.house_robber([1, 2, 3, 1]))
         self.assertEqual(22, DynamicProgramming.house_robber([2, 1, 9, 20, 1]))
         self.assertEqual(10, DynamicProgramming.house_robber([2, 1, 1, 7, 1, 1]))
-        self.assertEqual(4, dp.house_robber([1, 2, 3, 1]))
-        self.assertEqual(22, dp.house_robber([2, 1, 9, 20, 1]))
-        self.assertEqual(10, dp.house_robber([2, 1, 1, 7, 1, 1]))
 
     def test_fib(self):
         self.assertEqual(1, DynamicProgramming.fib(2))
@@ -21,7 +17,7 @@ class DynamicProgrammingTestCase(unittest.TestCase):
     def test_coin_change(self):
         self.assertEqual(3, DynamicProgramming.coin_change([1, 2, 5], 11))
         self.assertEqual(5, DynamicProgramming.coin_change([2, 3, 5, 7], 31))
-        self.assertEqual(6, DynamicProgramming.coin_change([1, 2, 5, 7], 31))
+        self.assertEqual(6, DynamicProgramming.coin_change([1, 2, 5, 7], 31))  # 5, 5, 7, 7, 7
         self.assertEqual(-1, DynamicProgramming.coin_change([2, 5, 7], 31))
         self.assertEqual(-1, DynamicProgramming.coin_change([2], 3))
         self.assertEqual(0, DynamicProgramming.coin_change([1], 0))
