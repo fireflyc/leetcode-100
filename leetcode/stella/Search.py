96a1cc06b24798831e1a779391f88f4791d61322
zstella <zhu.stella>
2021-05-17 11:22:02 +0800

    695. max-area-of-island

---
+++ leetcode/stella/Search.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def max_area_of_island(grid: List[List[int]]) -> int:
+    max_area = 0
+    searched = set()
+
+    def search(row, column):
+        if not grid[row][column]:
+            return set()
+        area = {(row, column)}
+        new_found = [(row, column)]
+        while new_found:
+            pos = new_found[0]
+            new_found = new_found[1:]
+            for r, c in [(pos[0]-1, pos[1]+0), (pos[0]+1, pos[1]+0), (pos[0]+0, pos[1]-1), (pos[0]+0, pos[1]+1)]:
+                if 0 <= r < len(grid) and 0 <= c < len(grid[r]) and grid[r][c] and (r, c) not in area:
+                    area.add((r, c))
+                    new_found.append((r, c))
+        return area
+
+    for r in range(0, len(grid)):
+        for c in range(0, len(grid[r])):
+            if (r, c) not in searched:
+                s = search(r, c)
+                max_area = max(len(s), max_area)
+                searched.update(s)
+    return max_area
+
+
+
---
+++ tests/stella/Search.py
@@ -0,0 +1,30 @@
+import unittest
+from leetcode.stella import Search
+
+
+class SearchTestCase(unittest.TestCase):
+
+    def test_max_area_of_island(self):
+        self.assertEqual(6, Search.max_area_of_island([
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+        ]))
+        self.assertEqual(2, Search.max_area_of_island([[1], [1]]))
+        self.assertEqual(6, Search.max_area_of_island([
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+        ]))
+        self.assertEqual(2500, Search.max_area_of_island([[1]*50]*50))
+        pass
