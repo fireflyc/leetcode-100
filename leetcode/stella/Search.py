fc6952c7a221076982cc40411ad9c682247c2501
zstella <zhu.stella>
2021-05-17 11:55:35 +0800

    escape a large maze

--- leetcode/stella/Search.py
+++ leetcode/stella/Search.py
@@ -2,9 +2,6 @@ from typing import List
 
 
 def max_area_of_island(grid: List[List[int]]) -> int:
-    max_area = 0
-    searched = set()
-
     def search(row, column):
         if not grid[row][column]:
             return set()
@@ -19,6 +16,7 @@ def max_area_of_island(grid: List[List[int]]) -> int:
                     new_found.append((r, c))
         return area
 
+    max_area, searched = 0, set()
     for r in range(0, len(grid)):
         for c in range(0, len(grid[r])):
             if (r, c) not in searched:
@@ -28,4 +26,6 @@ def max_area_of_island(grid: List[List[int]]) -> int:
     return max_area
 
 
+def escape_a_large_maze(blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
+    pass
 
--- tests/stella/Search.py
+++ tests/stella/Search.py
@@ -26,5 +26,9 @@ class SearchTestCase(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
         ]))
-        self.assertEqual(2500, Search.max_area_of_island([[1]*50]*50))
-        pass
+        self.assertEqual(2500, Search.max_area_of_island([[1] * 50] * 50))
+
+    def test_escape_a_large_maze(self):
+        self.assertEqual(False, Search.escape_a_large_maze(blocked=[[0, 1], [1, 0]], source=[0, 0], target=[0, 2]))
+        self.assertEqual(True, Search.escape_a_large_maze(blocked=[], source=[0, 0], target=[999999, 999999]))
+
