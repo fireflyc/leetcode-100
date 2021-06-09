ef8137759ffd1a90454c70ebb33af3aad21896d8
zstella <zhu.stella@outlook.com>
2021-06-09 23:33:31 +0800

    middle node of the linked list

--- leetcode/stella/LinkedList.py
+++ leetcode/stella/LinkedList.py
@@ -16,3 +16,15 @@ def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
         cur = next_one
         l1, l2 = l1.next if l1 else None, l2.next if l2 else None
     return root.next
+
+
+def middle_of_the_linked_list(head: ListNode) -> ListNode:
+    middle, end, middle_distance, end_distance = head, head, 0, 0
+    while end.next:
+        end = end.next
+        end_distance += 1
+        middle_pos = int((end_distance+1)/2) if end_distance % 2 else int(end_distance/2)
+        while middle_distance < middle_pos:
+            middle = middle.next
+            middle_distance += 1
+    return middle
--- tests/stella/LinkedList.py
+++ tests/stella/LinkedList.py
@@ -2,19 +2,27 @@ import unittest
 from leetcode.stella import LinkedList
 
 
+def get_list_node(data):
+    node = LinkedList.ListNode()
+    cur = node
+    for e in data:
+        next_one = LinkedList.ListNode(val=e)
+        cur.next = next_one
+        cur = cur.next
+    return node.next
+
+
 class LinkedListTestCase(unittest.TestCase):
     def test_add_two_numbers(self):
-        l1, l2, expect = [2, 4, 3], [5, 6, 4], [7, 0, 8]
-        node1, node2 = LinkedList.ListNode(), LinkedList.ListNode()
-        for node, data in [(node1, l1), (node2, l2)]:
-            cur = node
-            for e in data:
-                next_one = LinkedList.ListNode(val=e)
-                cur.next = next_one
-                cur = cur.next
-        ret, actual = LinkedList.add_two_numbers(l1=node1.next, l2=node2.next), []
+        ret, actual = LinkedList.add_two_numbers(l1=get_list_node([2, 4, 3]), l2=get_list_node([5, 6, 4])), []
         while ret:
             actual.append(ret.val)
             ret = ret.next
-        self.assertEqual(expect, actual)
+        self.assertEqual([7, 0, 8], actual)
+
+    def test_middle_of_the_linked_list(self):
+        node = LinkedList.middle_of_the_linked_list(get_list_node([1, 2, 3, 4, 5]))
+        self.assertEqual(3, node.val)
+        node = LinkedList.middle_of_the_linked_list(get_list_node([1, 2, 3, 4, 5, 6]))
+        self.assertEqual(4, node.val)
 
