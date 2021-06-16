0e24ea7ac98c1a14b00542c11f0fc9a3dbf54eda
zstella <zhu.stella@outlook.com>
2021-06-16 17:21:44 +0800

    rotate matrix

--- leetcode/stella/Array.py
+++ leetcode/stella/Array.py
@@ -9,3 +9,12 @@ def find_the_distance_value_between_two_arrays(arr1: List[int], arr2: List[int],
                 total -= 1
                 break
     return total
+
+
+def rotate_matrix_lcci(matrix: List[List[int]]) -> None:
+    for i in range(0, len(matrix)):
+        for j in range(i, len(matrix[i])):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(0, len(matrix)):
+        for j in range(0, int(len(matrix[0])/2)):
+            matrix[i][j], matrix[i][len(matrix[0])-1-j] = matrix[i][len(matrix[0])-1-j], matrix[i][j]
--- tests/stella/Array.py
+++ tests/stella/Array.py
@@ -6,3 +6,11 @@ class ArrayTestCase(unittest.TestCase):
     def test_find_the_distance_value_between_two_arrays(self):
         find_distance = Array.find_the_distance_value_between_two_arrays
         self.assertEqual(2, find_distance(arr1=[4, 5, 8], arr2=[10, 9, 1, 8], d=2))
+
+    def test_rotate_matrix_lcci(self):
+        data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        Array.rotate_matrix_lcci(data)
+        self.assertEqual([[7, 4, 1], [8, 5, 2], [9, 6, 3]], data)
+        data = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+        Array.rotate_matrix_lcci(data)
+        self.assertEqual([[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]], data)
