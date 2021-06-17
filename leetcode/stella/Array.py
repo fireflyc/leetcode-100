447ee8b24275be623f1829f14ee9682bc996fa63
zstella <zhu.stella@outlook.com>
2021-06-17 17:34:27 +0800

    jump game

--- leetcode/stella/Array.py
+++ leetcode/stella/Array.py
@@ -5,7 +5,7 @@ def find_the_distance_value_between_two_arrays(arr1: List[int], arr2: List[int],
     total = len(arr1)
     for n1 in arr1:
         for n2 in arr2:
-            if abs(n1-n2) <= d:
+            if abs(n1 - n2) <= d:
                 total -= 1
                 break
     return total
@@ -16,5 +16,40 @@ def rotate_matrix_lcci(matrix: List[List[int]]) -> None:
         for j in range(i, len(matrix[i])):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     for i in range(0, len(matrix)):
-        for j in range(0, int(len(matrix[0])/2)):
-            matrix[i][j], matrix[i][len(matrix[0])-1-j] = matrix[i][len(matrix[0])-1-j], matrix[i][j]
+        for j in range(0, int(len(matrix[0]) / 2)):
+            matrix[i][j], matrix[i][len(matrix[0]) - 1 - j] = matrix[i][len(matrix[0]) - 1 - j], matrix[i][j]
+
+
+def merge_intervals(intervals: List[List[int]]) -> List[List[int]]:
+    def merge(l1, l2):
+        if l1[1] < l2[0] or l2[1] < l1[0]:
+            return None
+        return [min(l1[0], l2[0]), max(l1[1], l2[1])]
+
+    total_len = len(intervals)
+    for i in range(0, total_len):
+        if intervals[i] is not None:
+            for j in range(0, total_len):
+                if j != i and intervals[j]:
+                    m = merge(intervals[i], intervals[j])
+                    if m:
+                        intervals[j] = m
+                        intervals[i] = None
+                        break
+    return [i for i in intervals if i is not None]
+
+
+def jump_game(nums: List[int]) -> bool:
+    reachable, gap = True, 1
+    for i in range(len(nums)-2, -1, -1):
+        if nums[i] >= gap:
+            reachable = True
+            gap = 1
+        if nums[i] == 0 or (nums[i] < gap and not reachable):
+            reachable = False
+            gap += 1
+    return reachable
+
+
+def container_with_most_water(height: List[int]) -> int:
+    pass
--- tests/stella/Array.py
+++ tests/stella/Array.py
@@ -14,3 +14,30 @@ class ArrayTestCase(unittest.TestCase):
         data = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
         Array.rotate_matrix_lcci(data)
         self.assertEqual([[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]], data)
+
+    def test_merge_intervals(self):
+        self.assertEqual([[1, 6], [8, 10], [15, 18]], Array.merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]]))
+        self.assertEqual([[1, 5]], Array.merge_intervals([[1, 4], [4, 5]]))
+        self.assertEqual([[0, 4]], Array.merge_intervals([[1, 4], [0, 1]]))
+        self.assertEqual([[2, 4], [5, 5]], Array.merge_intervals([[2, 3], [5, 5], [2, 2], [3, 4], [3, 4]]))
+
+    def test_jump_game(self):
+        self.assertEqual(True, Array.jump_game([2, 3, 1, 1, 4]))
+        self.assertEqual(False, Array.jump_game([3, 2, 1, 0, 4]))
+        self.assertEqual(True, Array.jump_game([2, 0]))
+        self.assertEqual(True, Array.jump_game([2, 0, 0]))
+        self.assertEqual(False, Array.jump_game([2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1,
+                                                 2, 6, 5, 3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1, 2, 0,
+                                                 1, 4, 6, 5, 8, 0, 7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0,
+                                                 9, 8, 4, 3, 0, 7, 7, 1, 9, 1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2,
+                                                 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6]))
+        with open('jump_game.json', 'r') as fr:
+            from json import load
+            data = load(fr)
+            self.assertEqual(False, Array.jump_game(data))
+
+    def test_container_with_most_water(self):
+        self.assertEqual(49, Array.container_with_most_water([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+        self.assertEqual(16, Array.container_with_most_water([4, 3, 2, 1, 4]))
+        self.assertEqual(1, Array.container_with_most_water([1, 1]))
+        self.assertEqual(2, Array.container_with_most_water([1, 2, 1]))
